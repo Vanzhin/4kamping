1107a6c5334a55c5eeaf73e91860dd33ad00fec7
Nikolay <nikolay.vanzhin@yandex.ru>
2021-04-05 12:18:09 +0500

    new-order create

--- лендинг/script.js
+++ лендинг/script.js
@@ -1,4 +1,4 @@
-Vue.component('search', {
+Vue.component('order', {
         props: {
             isTelNumOk: {
                 type: Boolean,
@@ -11,15 +11,15 @@ Vue.component('search', {
 
 
         },
-    
+
         template: `<div class="callback-tel-wrap" >
 
-            <div class="contact-info">
+            <from class="contact-info">
                 <div class="close" @click="closeForm"><i class="far fa-times-circle"></i></div>
-                <div  class="err-warning" v-bind:class="{hidden: !isTelNumOk}" v-bind:isTelNumOk='isTelNumOk'>Ошибка! Номер телефона должен состоять из цифр</div>
+                <div  class="err-warning" v-bind:class="{hidden: !isTelNumOk}" v-bind:isTelNumOk='isTelNumOk'>Ошибка! Номер телефона должен состоять из 11 цифр</div>
                 <input type="tel" id="your-tel" class="callback-input" placeholder="Ваш сотовый" v-on:input="handleInput">
-                <button class="sendit-btn header-sendit-btn" v-on:click="handleSearch">отправить</button>
-            </div>
+                <button class="sendit-btn header-sendit-btn" v-on:click="handleSearch" type="submit">отправить</button>
+            </form>
         </div>`,
         data() {
             return {
@@ -39,12 +39,63 @@ Vue.component('search', {
                 this.$emit('close-form', this.isFormActive);
             },
             changeIsNumOk() {
-            this.$emit('change', this.inputValue);
-        },
+                this.$emit('change', this.inputValue);
+            },
         }
 
-        
+
     }),
+    Vue.component('new-order', {
+        props: ['regexpTel'],
+
+        template: `<div class="callback-tel-wrap" >
+
+            <from action="#" class="contact-info">
+                <div class="close" @click="closeForm"><i class="far fa-times-circle"></i></div>
+                <div  class="warning"><span class="err" v-if="!isNumberCorrect()">Номер должен состоять из 11 цифр</span>
+<span class="ok" v-if="isNumberCorrect()">Ура! Все верно.</span></div>
+                <input type="tel" id="your-tel" class="callback-input" placeholder="Ваш сотовый" v-on:input="handleInput">
+                <button  
+v-bind:class="['sendit-btn', { disabled: !isNumberCorrect()}]"
+:disabled="!isNumberCorrect()"
+v-on:click="" type="submit"
+>отправить</button>
+
+            </form>
+        </div>`,
+        data() {
+            return {
+                inputValue: '',
+            };
+        },
+        methods: {
+            handleInput(e) {
+                this.inputValue = e.target.value;
+                console.log(this.inputValue);
+                this.isNumberCorrect();
+
+            },
+            isNumberCorrect() {
+                console.log(this.regexpTel);
+                if (this.regexpTel.test(this.inputValue)) {
+                    console.log("все верно");
+                    return true;
+                } else {
+                    console.log("не верно");
+
+                    return false
+                };
+            },
+            closeForm() {
+                this.$emit('close-form', this.isFormActive);
+            },
+
+        }
+
+
+    }),
+
+
     new Vue({
         el: '#app',
         data: {
